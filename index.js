aca2c903d291e9180b590f98dfc68f891d1156d7
seer <seer@nebula>
2021-07-23 14:55:53 -0400

    surface crypto lib

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 module.exports = {
 	config: require("./lib/config.js"),
 	crudDao: require("./lib/crudDao.js"),
+	cryptoCore: require("./lib/cryptoCore.js"),
 	csrf: require("./lib/csrf.js"),
 	logger: require("./lib/logger.js"),
 	stats: require("./lib/stats.js"),
